37a4087ee4d892dcaf0ac0c9f1250bd77a998614
jennifer gustavson <jengustavson@gmail.com>
2018-06-06 14:10:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,23 +12,23 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
 
 function accessElementInArray(array, index){
   return array[index]
-+}
-+
-+
-+function addElementToEndOfArray(array, element) {
-+  return [...array, element]
-+}
-+
-+function destructivelyAddElementToEndOfArray(array, element) {
-+  array.push(element)
-+  return array
-+}
-+
-+function destructivelyRemoveElementFromBeginningOfArray(array){
-+  array.shift()
-+  return array
-+}
-+
+}
+
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
