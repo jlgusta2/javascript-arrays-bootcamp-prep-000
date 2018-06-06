0ff2dbf3d2808886494680d49b5087c6cd9e5bf9
jennifer gustavson <jengustavson@gmail.com>
2018-06-06 14:09:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,13 +5,13 @@ function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
-+function destructivelyAddElementToBeginningOfArray (array, element) {
-+  array.unshift(element);
-+  return array
-+}
-+
-+function accessElementInArray(array, index){
-+  return array[index]
+function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
 +}
 +
 +
@@ -29,24 +29,18 @@ function addElementToBeginningOfArray(array, element) {
 +  return array
 +}
 +
-+function removeElementFromBeginningOfArray(array){
-+  return array.slice(1)
-+}
-+
-+function destructivelyRemoveElementFromEndOfArray(array){
-+  array.pop()
-+  return array
-+}
-+
-+function removeElementFromEndOfArray(array){
-+  return array.slice(0, array.length - 1)
-+}
-+function removeElementFromEndOfArray(array) {
-+  return array.slice(0, array.length - 1)
-+}
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-H
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1)
+}
